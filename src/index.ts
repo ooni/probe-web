aa7a70f43b1cf759c145322c844f5095bea948db
Arturo Filastò <arturo@filasto.net>
2021-04-27 11:38:51 +0200

    Minimal PoC

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,62 @@ type GeoIPLookup = {
     asn: number,
     organization: string,
     country: string
-}
+};
+
+class Measurer {
+
+    src: string;
+    element: string;
+    startTime: number;
+    geoipLookup: GeoIPLookup;
+
+    constructor(element: string, src: string, geoipLookup: GeoIPLookup) {
+        this.element = element
+        this.src = src
+        this.geoipLookup = geoipLookup
+    }
+
+    measure() {
+        let el = document.createElement(this.element)
+        el.setAttribute('src', this.src)
+        el.addEventListener('load', () => {
+            const endTime = Date.now()
+            this.submitResult(endTime)
+        })
+        el.addEventListener('error', (err) => {
+            const endTime = Date.now()
+            this.submitError(endTime, err)
+        })
+        this.startTime = Date.now()
+        document.getElementById('measurer').appendChild(el)
+    }
+
+    submitResult(endTime: number) {
+        console.log('OK')
+        console.log({
+            'input': this.src,
+            'result': 'ok',
+            'result_failure': null,
+            'test_runtime': endTime - this.startTime,
+            'probe_asn': this.geoipLookup.asn,
+            'probe_cc': this.geoipLookup.country,
+            'probe_network_name': this.geoipLookup.organization,
+        })
+    }
+
+    submitError(endTime: number, error) {
+        console.log('Error')
+        console.log({
+            'input': this.src,
+            'result': 'error',
+            'result_failure': error,
+            'test_runtime': endTime - this.startTime,
+            'probe_asn': this.geoipLookup.asn,
+            'probe_cc': this.geoipLookup.country,
+            'probe_network_name': this.geoipLookup.organization,
+        })
+    }
+};
 
 async function lookupGeoIP() : Promise<GeoIPLookup> {
     try {
@@ -23,9 +78,37 @@ async function lookupGeoIP() : Promise<GeoIPLookup> {
     }
 }
 
+type InputSpec = {
+    element: string,
+    src: string
+};
+
+type InputList = Array<InputSpec>;
+
+async function lookupInputs() : Promise<InputList> {
+    return new Promise((resolve, reject) => {
+        resolve([
+            {
+                src: 'https://twitter.com/favicon.ico',
+                element: 'img' 
+            },
+            {
+                src: 'https://thepiratebay.se/favicon.ico',
+                element: 'img' 
+            }
+        ])
+    })
+}
+
 async function main() {
-    const geoIP : GeoIPLookup = await lookupGeoIP()
-    console.log(geoIP)
+    const geoIPlookup : GeoIPLookup = await lookupGeoIP()
+    console.log(geoIPlookup)
+    const inputs = await lookupInputs()
+    console.log(inputs)
+    inputs.forEach(i => {
+        const m = new Measurer(i.element, i.src, geoIPlookup)
+        m.measure()
+    })
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
