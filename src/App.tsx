2b79207f6ffc689e6491761b5216247a0491c5c4
Arturo Filastò <arturo@filasto.net>
2021-12-21 19:14:25 +0200

    Add very basic styling via ooni-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,52 @@
 import React, { useState, useRef, useEffect } from 'react'
 
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import styled from 'styled-components'
+
+import { theme } from 'ooni-components'
+
+import { 
+    Heading,
+    Text
+} from 'ooni-components'
+
 import Runner from './Runner'
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    text-rendering: geometricPrecision;
+    box-sizing: border-box;
+  }
+  body, html {
+    margin: 0;
+    padding: 0;
+    font-family: "Fira Sans";
+    font-size: 14px;
+    height: 100%;
+    background-color: #ffffff;
+  }
+`
+
+const HeroUnit = styled.div`
+background: linear-gradient(
+    319.33deg,
+    ${props => props.theme.colors.blue9} 39.35%,
+    ${props => props.theme.colors.base} 82.69%),
+    ${props => props.theme.colors.base};
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+  padding-bottom: 16px;
+  padding-top: 16px;
+`
+
+const LogContainer = styled.div`
+    background-color: ${props => props.theme.colors.gray8};
+    color: ${props => props.theme.colors.white};
+    padding-left: 32px;
+    height: 300px;
+    overflow: scroll;
+`
 
 const App = () => {
     const [ logs, setLog ] = useState([])
@@ -8,6 +54,7 @@ const App = () => {
     const [ progress, setProgress] = useState(0)
 
     const runnerRef = useRef()
+    const logEndRef = useRef()
 
     const onLog = (l) => {
         setLog(logs => [...logs, l])
@@ -28,11 +75,24 @@ const App = () => {
         runner.run()
     }, [])
 
+    useEffect(() => {
+        logEndRef.current.scrollIntoView({ behavior: 'smooth' })
+    })
+
     return (
-        <div className="App">
-            <h2>{status}</h2>
-            {logs.map(l => <p>{l.toString()}</p>)}
-        </div>
+        <ThemeProvider theme={theme}>
+            <GlobalStyle />
+            <div className="App">
+                <HeroUnit>
+                    <Heading h={2} px={4} color='white'>{status}</Heading>
+                </HeroUnit>
+                <LogContainer>
+                    {logs.map(l => <Text>{l.toString()}</Text>)}
+                    <div ref={logEndRef}></div>
+                </LogContainer>
+            </div>
+        </ThemeProvider>
+
     );
 }
 
