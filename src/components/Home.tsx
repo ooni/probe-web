bc680323df0c7404b14651acea11a281c7353f96
Arturo Filastò <arturo@filasto.net>
2022-05-19 18:31:27 +0200

    Add support for using the check-in API geoip information

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react'
 import { useState } from 'react'
 
-import styled from 'styled-components'
-
 import { 
     Container, Text, Button, Heading,
     Flex, Box
--- src/components/Runner.ts
+++ src/components/Runner.ts
@@ -1,16 +1,16 @@
-
 const software_name = 'ooniprobe-web'
 const software_version = '0.0.1'
 const data_format_version = '0.2.0'
 
 type GeoIPLookup = {
-    asn: number,
-    organization: string,
-    country: string
+    probe_cc: string,
+    probe_asn: string,
+    probe_network_name: string,
 };
 
 type TestKeys = {
-    result: string
+    result: string,
+    load_time_ms: number
 }
 
 type Measurement = {
@@ -39,37 +39,28 @@ export type RunnerOptions = {
     urlLimit: number
 }
 
-async function lookupGeoIP() : Promise<GeoIPLookup> {
-    try {
-        const resp = await fetch('https://get.geojs.io/v1/ip/geo.json')
-        const j = await resp.json()
-        const g: GeoIPLookup = {
-            asn: j['asn'],
-            organization: j['organization_name'],
-            country: j['country_code']
-        }
-        return g
-
-    } catch (err) {
-        console.log('failed with error', err)
-        throw err
-    }
-}
-
 type InputList = Array<string>;
 
 async function measure(input: string) : Promise<[string, number]> {
     let result : string;
     const start_time = performance.now()
     try {
-        await fetch(input, {
+        let resp = await fetch(input, {
             method: 'GET',
             mode: 'no-cors',
             cache: 'no-store',
-            credentials: 'omit'
+            credentials: 'omit',
+            referrer: 'no-referrer',
+            referrerPolicy: 'no-referrer',
+            redirect: 'follow',
+            // XXX investigate if using this property: https://developer.mozilla.org/en-US/docs/Web/API/AbortSignal
+            // signal
+            headers: {"Accept-Language": "en-US,en;q=0.8", "Accept-Charset": "ISO-8859-1,utf-8;q=0.7,*;q=0.3"}, 
         })
+        console.log(resp)
         result = 'ok'
     } catch(error) {
+        console.log('failed to measure with error', error)
         result = 'error'
     }
     return [result, performance.now() - start_time]
@@ -112,13 +103,11 @@ class Runner {
         this.apiBaseURL = 'https://ams-pg-test.ooni.org'
     }
 
-    async lookupInputs(): Promise<InputList> {
+    async checkinRequest(): Promise<InputList> {
         const req = {
             "charging": true,
             "on_wifi": true,
             "platform": "browser",
-            "probe_asn": `AS${this.geoip.asn}`,
-            "probe_cc": this.geoip.country,
             "run_type": "timed",
             "software_name": software_name,
             "software_version": software_version,
@@ -136,24 +125,25 @@ class Runner {
             body: JSON.stringify(req)
         })
         const j = await resp.json()
-        try {
-            let urls = j['tests']['web_connectivity']['urls'].map(u => u.url)
-            if (this.urlLimit !== 0) {
-                urls = urls.slice(0, this.urlLimit)
-            }
-            return urls
-        } catch (e) {
-            console.log('failed to lookup inputs', e)
-            throw e
+        this.geoip = {
+            probe_asn: j["probe_asn"],
+            probe_cc: j["probe_cc"],
+            probe_network_name: j["probe_network_name"]
         }
+
+        let urls = j['tests']['web_connectivity']['urls'].map(u => u.url)
+        if (this.urlLimit !== 0) {
+            urls = urls.slice(0, this.urlLimit)
+        }
+        return urls
     }
 
     async openReport(test_start_time : string, test_name : string, test_version : string) : Promise<string> {
         const req = {
             "data_format_version": data_format_version,
             "format": "json",
-            "probe_asn": `AS${this.geoip.asn}`,
-            "probe_cc": this.geoip.country,
+            "probe_asn": this.geoip.probe_asn,
+            "probe_cc": this.geoip.probe_cc,
             "software_name": software_name,
             "software_version": software_version,
             "test_name": test_name,
@@ -191,15 +181,14 @@ class Runner {
         return j['measurement_uid']
     }
 
-    async run() : Promise<> {
+    async run() : Promise<Array<Measurement>> {
         let results = []
 
         const test_name = 'browser_web'
         const test_version = '0.1.0'
 
-        this.geoip = await lookupGeoIP()
         this.onLog('looked up geoIP', this.geoip)
-        const inputs = await this.lookupInputs()
+        const inputs = await this.checkinRequest()
         this.onLog('looked up inputs', inputs)
 
         const test_start_time = new Date().toISOString().replace('T', ' ').slice(0, 19)
@@ -221,29 +210,32 @@ class Runner {
                 data_format_version,
                 report_id,
                 measurement_start_time,
-                probe_asn: `AS${this.geoip.asn}`,
-                probe_cc: this.geoip.country,
-                probe_network_name: this.geoip.organization,
+                probe_asn: this.geoip.probe_asn,
+                probe_cc: this.geoip.probe_cc,
+                probe_network_name: this.geoip.probe_network_name,
                 input: i,
-                test_runtime: -1,
-                test_keys: { result: "" }
+                test_runtime: 0,
+                test_keys: { result: "", load_time_ms: 0 }
             };
             this.onStatus(`Measuring ${i}`)
             this.onLog(`Measuring ${i}`)
             let [result, runtime] = await measure(i)
-            measurement.test_runtime = runtime
-            measurement.test_keys = { 'result': result }
+            measurement.test_runtime = runtime / 1000
+            measurement.test_keys = {
+                'result': result,
+                'load_time_ms': runtime,
+            }
             this.onLog(`Measured: ${JSON.stringify(measurement)}`)
 
             if (this.uploadResults === true) {
                 const msmtUID = await this.submitMeasurement(measurement)
                 this.onLog(`Submitted measurement with UID ${msmtUID}`)
             }
-
             results.push(measurement)
         }
         this.onProgress(100)
         this.onResults(results)
+        return results
     }
 }
 
