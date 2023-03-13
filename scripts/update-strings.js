2b162da58b9c38356a2428d48a4448c7db35834d
Arturo Filastò <arturo@filasto.net>
2023-03-13 17:45:27 +0100

    Add more strings

--- scripts/update-strings.js
+++ scripts/update-strings.js
@@ -6,7 +6,6 @@ const DST_FILE = "lang/en.json"
 const CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSEwBVQdcdZ5FKaAqtYJA5zrtDUijGY-HFb7tpWT3KwOb9ISdru1hOXaAcNgyn55xc4dg0ykdnH2XTZ/pub?gid=0&single=true&output=csv"
 
 const main = async () => {
-
   const resp = await axios.get(CSV_URL)
   const j = parse(resp.data, {from: 2})
     .reduce(
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -18,6 +18,9 @@ import { Input, Label } from "@rebass/forms";
 
 import { useNavigate } from "react-router-dom";
 
+import FormattedMarkdown from './FormattedMarkdown'
+import { FormattedMessage } from "react-intl";
+
 const AlertBanner = styled.div`
 padding: 8px 16px;
 border-radius: 4px;
@@ -50,26 +53,29 @@ const Home = ({ onResetInformedConsent }) => {
 
   return (
     <Container pt={2}>
-      <Heading>Welcome to OONI Probe Web</Heading>
+      <Heading>
+          <FormattedMessage id="Home.Title" />
+      </Heading>
 
       {browserName == "firefox" && <AlertBanner>
-        <Text>You appear to be using firefox.
-          For accurate measurements, please be sure to <a href="https://support.mozilla.org/en-US/kb/enhanced-tracking-protection-firefox-desktop#w_strict-enhanced-tracking-protection">disable strict enhanced tracking protection</a> for this site.
+        <Text>
+          <FormattedMarkdown id="Home.FirefoxWarning" />
         </Text>
       </AlertBanner>
       }
 
-      <Text>This is an experimental version of OONI Probe which checks for websites blocking by using your web browser.
-        Keep in mind that tests run using probe-web cannot be as accurate as those run with
-        the <a href="https://ooni.org/install/mobile">OONI Probe mobile</a> or <a href="https://ooni.org/install/desktop">desktop apps</a>.
-      </Text>
+      <Text>
+      <FormattedMarkdown id="Home.MainDescription" />
+     </Text>
 
-      <Text>OONI Probe web is not designed to be a replacement for <a href="https://ooni.org/install/">OONI Probe</a> on mobile and desktop platforms.</Text>
-
-      <Heading h={3}>Settings</Heading>
+      <Heading h={3}>
+        <FormattedMarkdown id="Settings.Title" />
+      </Heading>
       <Flex flexWrap="wrap">
         <Box width={1 / 2}>
-          <Label>URL Limit (0 for no limit)</Label>
+          <Label>
+            <FormattedMessage id="Settings.URLLimit" />
+          </Label>
           <Input defaultValue={0} {...register("urlLimit")} />
         </Box>
         <Box width={1 / 2} style={{ display: "none" }}>
